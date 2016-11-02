9363673eac2733f1b33f0470462dbf100eae615c
marvan_t <marvan_t@hotmail.com>
2016-11-02 09:38:15 -0700

    migrate toy

--- mypets/db/schema.rb
+++ mypets/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102162655) do
+ActiveRecord::Schema.define(version: 20161102163508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,13 @@ ActiveRecord::Schema.define(version: 20161102162655) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "toys", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "pet_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["pet_id"], name: "index_toys_on_pet_id", using: :btree
+  end
+
+  add_foreign_key "toys", "pets"
 end
