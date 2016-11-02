55e271a323da46feea2850f73a9079601d056cd4
marvan_t <marvan_t@hotmail.com>
2016-11-02 10:10:32 -0700

    modify rout and controller, and create index view page for pets

--- mypets/app/controllers/pets_controller.rb
+++ mypets/app/controllers/pets_controller.rb
@@ -1,2 +1,6 @@
 class PetsController < ApplicationController
+	def index
+		@pets = Pet.all
+	end
+
 end
--- mypets/config/routes.rb
+++ mypets/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pets do 
-  	resources :toys
+  get '/', to: 'pets#index'
+  resources :pets, only: [:index, :show] do 
+  	resources :toys, only: [:index, :show]
   end
 end
