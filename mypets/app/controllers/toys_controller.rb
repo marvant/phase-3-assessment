6f023c9dc475338420575764c2860c5e45160c64
marvan_t <marvan_t@hotmail.com>
2016-11-02 11:02:53 -0700

    get json response back

--- mypets/app/controllers/toys_controller.rb
+++ mypets/app/controllers/toys_controller.rb
@@ -1,6 +1,20 @@
 class ToysController < ApplicationController
 	def index
-		@toys = Toy.all
+			@pet = Pet.find(params[:pet_id])
+			@toys = @pet.toys
+			
+			# respond_to do |format|
+			# 	format.html
+			# 	format.json { render json: @pet }
+			# end
+
+			if request.xhr?
+				p 'getting here'
+				# render :partial => 'toy'
+				# render json: @pet
+				render json: @toys
+			end
+
 	end
 
 end
--- mypets/config/routes.rb
+++ mypets/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'pets#index'
+  get 'home', to: 'pets#index'
   resources :pets, only: [:index, :show] do 
   	resources :toys, only: [:index, :show]
   end
+
+  root 'pets#index'
 end
